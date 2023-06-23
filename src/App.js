6ac4ccdabad63220a2027e1ca42b704ffe9c3b75
Crescens <crescencio.ps.code@gmail.com>
2023-06-22 19:53:14 -0600

    quick fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import Hermes from "./pages/Hermes";
 import Login from "./pages/LogIn";
 import Register from "./pages/Register";
 import AboutUs from "./pages/AboutUs";
-import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Nav />
+        <div className="bg-black text-white w-full h-screen overflow-hidden">
           <Routes>
             <Route path="" element={<Home />} />
             <Route path="/Hermes" element={<Hermes />} />
---
+++ src/pages/LogIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Logo } from "./../Assets/Img/codex.png";
+
+export default function LogIn() {
+  return (
+    <>
+      <div className="text-white">
+        <h1>Hola desde login</h1>
+      </div>
+    </>
+  );
+}
